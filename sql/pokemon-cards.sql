5974fa869861a1a4aba4c8b9f96cb41b1f1c974d
meester-tom <tgreuter@novacollege.nl>
2026-02-11 15:46:31 +0100

    Change pokedex_number to string format in SQL insert
    Updated pokedex_number values to be strings instead of integers.

--- sql/pokemon-cards.sql
+++ sql/pokemon-cards.sql
@@ -7,12 +7,12 @@ CREATE TABLE cards (
 );
 
 INSERT INTO cards (name, pokedex_number, type, description) VALUES
-('Pikachu', 025, 'Elektrisch', 'Elektrisch type Pokémon'),
-('Charmander', 004, 'Vuur', 'Vuur type Pokémon'),
-('Squirtle', 007, 'Water', 'Water type Pokémon'),
-('Bulbasaur', 001, 'Gras/Gift', 'Gras/Gift type Pokémon'),
-('Mewtwo', 150, 'Psychisch', 'Psychisch type Pokémon'),
-('Charizard', 006, 'Vuur/Vliegend', 'Vuur/Vliegend type Pokémon'),
-('Blastoise', 009, 'Water', 'Water type Pokémon'),
-('Venusaur', 003, 'Gras/Gift', 'Gras/Gift type Pokémon'),
-('Articuno', 144, 'IJs/Vliegend', 'IJs/Vliegend type Pokémon');
+('Pikachu', '025', 'Elektrisch', 'Elektrisch type Pokémon'),
+('Charmander', '004', 'Vuur', 'Vuur type Pokémon'),
+('Squirtle', '007', 'Water', 'Water type Pokémon'),
+('Bulbasaur', '001', 'Gras/Gift', 'Gras/Gift type Pokémon'),
+('Mewtwo', '150', 'Psychisch', 'Psychisch type Pokémon'),
+('Charizard', '006', 'Vuur/Vliegend', 'Vuur/Vliegend type Pokémon'),
+('Blastoise', '009', 'Water', 'Water type Pokémon'),
+('Venusaur', '003', 'Gras/Gift', 'Gras/Gift type Pokémon'),
+('Articuno', '144', 'IJs/Vliegend', 'IJs/Vliegend type Pokémon');
